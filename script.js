f1feba1d52cc2282c04d75726078ac8630e1c709
Pavel <level199@yandex.ru>
2021-08-28 20:24:35 +0300

    Выполнил первое задание

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+let price = prompt("Ваш бюджет?");
+let name = prompt("Название вашего магазина?");
+let mainList = {
+    budjet: price,
+    shopName: name,
+    shopGoods: [],
+    employers: {},
+    open: false
+}
 
+mainList.shopGoods[0] = prompt("Какой тип товаров будем продавать?");
+mainList.shopGoods[1] = prompt("Какой тип товаров будем продавать?");
+mainList.shopGoods[2] = prompt("Какой тип товаров будем продавать?");
+
+console.log(price / 30);
+
+console.log(mainList.shopGoods);
